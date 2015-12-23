e64553c8db4f200785ff03e47fb39d4235a97135
vframbach <vframbach@gmail.com>
2015-12-23 18:52:15 -0500

    added posts, edit and delete works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,43 +1,64 @@
 class PostsController < ApplicationController
 
+	before_filter :get_trip
+
+	def get_trip
+		@trip = Trip.find(params[:trip_id])
+	end
+
 	def index
 		@posts = Post.all
 	end
 
 	def new 
-		@post = Post.new
+		@post = Post.new(:trip => @trip)
 	end
 
 	def create
-		@post = posts.new(post_params)
+		@post = Post.new(post_params)
 
 		if @post.save
-			redirect_to post_path(@post)
+			redirect_to trip_post_path(@trip, @post)
 		else
 			flash[:error] = @post.errors.full_messages.join(", ")
-			redirect_to new_post_path
+			redirect_to new_trip_post_path
 		end
 	end
 
 	def show
+		post_id = params[:id]
+
+		@post = Post.find_by_id(post_id)
 	end
 
 	def edit
+		post_id = params[:id]
+
+		@post = Post.find_by_id(post_id)
 	end
 
 	def update
+		post_id = params[:id]
+
+		@post = Post.find_by_id(post_id)
+
 		if @post.update_attributes(post_params)
 			flash[:notice] = "Successfully updated post."
-			redirect_to post_path(@post)
+			redirect_to trip_post_path(@trip, @post)
 		else
 			flash[:notice] = @post.erros.full_messages.join(", ")
-			redirect_to edit_post_path(@post)
+			redirect_to edit_trip_post_path(@trip, @post)
 		end
 	end
 
 	def destroy
+		post_id = params[:id]
+
+		@post = Post.find_by_id(post_id)
+
 		@post.destroy
 		flash[:notice] = "Successfully deleted post."
+		redirect_to trip_path(@trip)
 	end
 
 	private
